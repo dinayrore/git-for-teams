1a4c9dccd164e081ff25cddeaa8d99f574de6ac7
Kristine Teichmann <kteich88@gmail.com>
2018-09-22 08:08:00 -0400

    updated git_practice.rb with material learned

--- git_practice.rb
+++ git_practice.rb
@@ -1,1 +1,34 @@
 # write some code and practice commiting your work to Github!
+# variables
+x = 27 # integer 
+name = "Kristine" # string
+
+# expressions - using variable assignments to make equations
+x = 20
+y = 7
+x + y
+
+# concatenation
+name = "Kristine"
+"Hello my name is " + name
+
+# string interpolation
+name = "Kristine"
+print "What is your name? "
+name = gets.chomp 
+puts "Hello #{name}!"
+
+# loops
+command = ''
+while command != 'bye'
+  puts command
+  command = gets.chomp
+end
+puts 'Come again soon!'
+
+# arrays
+[] # this is an empty Array
+array = Array.new # this is also an empty array
+names = ['Kristine', 'Rene', 'Shu', 'Alik', 'Victor', 'John']
+names << 'Gracias'
+names.push 'Jennifer'
